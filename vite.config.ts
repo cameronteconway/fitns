6b5c278d18783bb5ba4d5f8e96fdf4a5f849d9e6
cameronteconway <cameronteconway@outlook.com>
2023-07-19 14:38:51 +0100

    Commit: 2023-07-19 14:38:51

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: true,
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                app: './index.html',
+                'service-worker': './service-worker.ts',
+            },
+            output: {
+                entryFileNames: (assetInfo) => {
+                    return assetInfo.name === 'service-worker'
+                        ? '[name].js' // put service worker in root
+                        : 'assets/js/[name]-[hash].js'; // others in `assets/js/`
+                },
+            },
+        },
+    },
+});
